9f6889315809ae939a03ec2534f09c04c626e494
Gorani92 <peses0326@gmail.com>
2021-07-10 10:36:01 +0900

    New cheerup server upload

---
+++ cheerup/src/main/java/com/cheerup/cheerup/CheerupApplication.java
@@ -0,0 +1,13 @@
+package com.cheerup.cheerup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheerupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheerupApplication.class, args);
+    }
+
+}
