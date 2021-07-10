a45c8d4a6c8950382e0e2356d5a4df23b6421dc2
Gorani92 <peses0326@gmail.com>
2021-07-10 20:59:20 +0900

    모든 기능 구현 + 생성,수정시간 표기

--- cheerup/src/main/java/com/cheerup/cheerup/CheerupApplication.java
+++ cheerup/src/main/java/com/cheerup/cheerup/CheerupApplication.java
@@ -3,12 +3,14 @@ package com.cheerup.cheerup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CheerupApplication {
 
