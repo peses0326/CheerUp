b9189109a5453581a726f6a2fe0b6a6d91dceafc
KSY <kisy324@naver.com>
2021-07-12 13:21:41 +0900

    토큰 만들어봄

--- cheerup/src/main/java/com/cheerup/cheerup/security/UserDetailsServiceImpl.java
+++ cheerup/src/main/java/com/cheerup/cheerup/security/UserDetailsServiceImpl.java
@@ -2,16 +2,17 @@ package com.cheerup.cheerup.security;
 
 import com.cheerup.cheerup.model.User;
 import com.cheerup.cheerup.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    @Autowired
     private UserRepository userRepository;
 
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
