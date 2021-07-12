ae438e4a0e891835e732cfbad5ffaa9152eb8c4e
KSY <kisy324@naver.com>
2021-07-12 19:50:48 +0900

    1. 로그인 기능
    2.
    POST "/user/signup" 응답
    {
    	"valid_username": "ID가 공백이거나, ID가 3글자 이상 12글자 이하의 값이 아닐 때의 오류",
    	"valid_password": "PW가 공백이거나, 비밀번호가 영문 대문자, 소문자, 숫자, 특수기호가 적어도 1개씩 포함된 4글자 이상 16글자 이하의 값이 아닐 때의 오류",
    	"errors": "부적절한 ID(null or admin일 때), 중복ID, PW와 ID에 중복값이 포함되었을 때, PW확인이 틀렸을 때의 오류"
    	"msg": "회원가입 완료"
    }
    
    POST "/user/login" 요청
    {
    	"username": "유저이름",
    	"password": "aaAA11!!"
    }
    
    GET "/user/session" 응답
    {
    	"username": "현재로그인한이름",
    	"role": "USER거나ADMIN"
    }
    ->username과 role이 null일 경우 비로그인상태 or 세션만료

--- cheerup/src/main/java/com/cheerup/cheerup/model/User.java
+++ cheerup/src/main/java/com/cheerup/cheerup/model/User.java
@@ -4,10 +4,8 @@ import lombok.*;
 
 import javax.persistence.*;
 
-@Builder
-@Setter
-@Getter // get 함수를 일괄적으로 만들어줍니다.
-@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Data
 @NoArgsConstructor // 기본 생성자를 만들어줍니다.
 @Entity // DB 테이블 역할을 합니다.
 public class User extends Timestamped {
